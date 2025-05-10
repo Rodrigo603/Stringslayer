d6b90fa34c1469ea576efc6bf08c6d13ebf158bf
Rodrigo603 <Rodrigo6031@gmail.com>
2025-05-10 19:22:18 -0300

    atualizando com a esteira da guitarra

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include "raylib.h"
-#include "sprites.h"
 
 int main(void) 
 {
@@ -7,13 +6,34 @@ int main(void)
     const int screenHeight = 600;
 
     InitWindow(screenWidth, screenHeight, "StringSlayer");
-    Texture2D background = LoadTexture("assets/String Slayer.png");
 
+    Texture2D background = LoadTexture("assets/String Slayer.png");
+    Texture2D esteira = LoadTexture("assets/esteira.png");
+
+    Rectangle playButton = { 300, 250, 200, 50 };  
+
+    Color buttonColor = DARKGRAY;
+
+    
+    SetTargetFPS(60);
+
+    bool isPlaying = false;  
+
+   
     while (!WindowShouldClose()) 
     {
-        BeginDrawing();
-            ClearBackground(BLACK);
+       
+        if (!isPlaying && CheckCollisionPointRec(GetMousePosition(), playButton) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            isPlaying = true;
+        }
 
+        BeginDrawing();
+
+        ClearBackground(BLACK); 
+
+        if (!isPlaying) 
+        {
             DrawTexturePro(
                 background,
                 (Rectangle){ 0, 0, (float)background.width, (float)background.height },
@@ -23,10 +43,35 @@ int main(void)
                 WHITE
             );
 
+            DrawRectangleRec(playButton, buttonColor); 
+            DrawText("Jogar", playButton.x + 60, playButton.y + 15, 30, WHITE); 
+        }
+        else 
+        {
+            float scale = 0.41f; 
+            float width = (float)(esteira.width * scale);
+            float height = (float)(esteira.height * scale); 
+
+            float posX = (screenWidth - width) / 2.0f;
+            float posY = (screenHeight - height) / 2.0f;
+
+            Rectangle targetRect = { posX, posY, width, height };
+            DrawTexturePro(
+                esteira,
+                (Rectangle){ 0, 0, (float)esteira.width, (float)esteira.height },
+                targetRect,
+                (Vector2){ 0, 0 },
+                0.0f,
+                WHITE
+            );
+        }
+
         EndDrawing();
     }
 
     UnloadTexture(background);
+    UnloadTexture(esteira);
+
     CloseWindow();
 
     return 0;
